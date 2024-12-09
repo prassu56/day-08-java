aba6ef17d17f4b0d07f4d928472dc1cda4d7029d
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-09 15:10:23 +0530

    Add files via upload

---
+++ Elementsinmatrixform.java
@@ -0,0 +1,37 @@
+/*
+2-rows
+3-columns
+2*3=6elements we have to give as input
+1
+2
+3
+4
+5
+6
+1 2 3 
+4 5 6
+*/
+ import java.util.*;
+public class Elementsinmatrixform{
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+    int r=sc.nextInt();
+    int c=sc.nextInt();
+    int arr[][]=new int[r][c];
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+        arr[i][j]=sc.nextInt();
+      }
+    }
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+        System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
+    }
+  }
