903a071a30713196e4aabf172b2454119355d19d
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-09 15:05:21 +0530

    Add files via upload

---
+++ Matrix2d.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Matrix2d{
+    public static void main(String[]args){
+        //initialization of 2D array//
+        int a[][]={{1,2},{3,4}};
+        //accessing 1Darray in 2D array//
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
